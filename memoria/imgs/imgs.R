273ddac50c9f920250911b97aa3451e49ceb0b0a
ncordon <nacho.cordon.castillo@gmail.com>
2017-08-07 21:51:00 +0200

    Corrige introducción al desbalanceo y añade imagen

---
+++ memoria/imgs/imgs.R
@@ -0,0 +1,19 @@
+############################################################################
+# Gráficas del proyecto
+#
+# Script para generar algunas de las gráficas del proyecto de fin de carrera
+############################################################################
+#devtools::install_github('ncordon/imbalance')
+library(imbalance)
+library(ggplot2)
+data(ecoli1)
+
+colorPalette <-  c("#009E73", "#D55E00", "#CC79A7")
+ggplot(ecoli1, aes_string("Gvh", "Mcg", col = "Class")) + 
+      geom_point(alpha = 1) +
+      scale_color_manual(values = colorPalette) +
+      theme(panel.background = element_blank())
+
+
+ggsave(filename="desbalanceo.png",width=7, height=5)
+
