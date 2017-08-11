03dc48e995574ad3012fa515b0eaa4e607ba2b4a
ncordon <nacho.cordon.castillo@gmail.com>
2017-08-11 04:18:30 +0200

    Añade descripción y teoría para el algoritmo RWO

---
+++ memoria/codelst/rwo.R
@@ -0,0 +1,17 @@
+newSamples <- lapply(minority, function(x){
+  # If attribute is numeric, generate new minority sample 
+  # preserving mean and variance of existent samples
+  if(is.numeric(x)){
+    variance <- stats::var(x)
+    x - variance/sqrt(n) * scaleFactors
+    
+    # Else if attribute is not numeric, make a roulette 
+    # out of possible values for the attribute and their frequency
+  } else{
+    dist <- table(x)
+    distValues <- names(dist)
+    distProbs <- unname(dist)
+    sample(distValues, length(x) * iterPerInstance, 
+           replace = T, prob = distProbs)
+  }
+})
