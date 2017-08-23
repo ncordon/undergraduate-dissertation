1df9153b9289a936169ff0c410dac624d1069305
ncordon <nacho.cordon.castillo@gmail.com>
2017-08-23 17:44:37 +0200

    Críticas a algunos algoritmos + fix de referencias

--- memoria/codelst/rwo.R
+++ memoria/codelst/rwo.R
@@ -1,17 +1,19 @@
 newSamples <- lapply(minority, function(x){
-  # If attribute is numeric, generate new minority sample 
-  # preserving mean and variance of existent samples
-  if(is.numeric(x)){
-    variance <- stats::var(x)
-    x - variance/sqrt(n) * scaleFactors
-    
-    # Else if attribute is not numeric, make a roulette 
+  # If attribute is continuous, generate new minority
+  # sample preserving mean and variance of existent samples
+  scaleFactors <- stats::rnorm(nrow(minority) * iterations,
+                               mean = 0, sd = 1)
+
+  if(class(x) == "numeric"){
+    variance <- (m-1)/m * stats::var(x)
+    x - variance/sqrt(m) * scaleFactors
+
+    # Else if attribute is not numeric, make a roulette out
     # of possible values for the attribute and their frequency
   } else{
     dist <- table(x)
     distValues <- names(dist)
     distProbs <- unname(dist)
-    sample(distValues, length(x) * iterPerInstance, 
-           replace = T, prob = distProbs)
+    sample(distValues, length(x) * iterations, replace = T, prob = distProbs)
   }
 })
